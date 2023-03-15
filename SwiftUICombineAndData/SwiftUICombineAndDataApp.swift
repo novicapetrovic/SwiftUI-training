cab0f712465c4cfb6525e6310a87a3056a7edf32
Nov Petrović <novicapetrovic97@gmail.com>
2023-03-15 16:17:10 +0000

    Initial Commit

---
+++ SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by Nov Petrović on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUICombineAndDataUITests/SwiftUICombineAndDataUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUICombineAndDataUITestsLaunchTests.swift
+//  SwiftUICombineAndDataUITests
+//
+//  Created by Nov Petrović on 15/03/2023.
+//
+
+import XCTest
+
+final class SwiftUICombineAndDataUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
